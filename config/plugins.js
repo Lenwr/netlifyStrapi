5e17a9ac9e468e190f69b3c23cafb4bcb697add2
Lenwr <saidabou.lateam@gmail.com>
2023-03-12 16:59:43 +0100

    netlifyConfig

--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,17 @@
 module.exports = ({ env }) => ({
   'netlify-deployments': {
     enabled: true,
+    config: {
+      accessToken: env('NETLIFY_DEPLOYMENTS_PLUGIN_ACCESS_TOKEN'),
+      sites: [
+        {
+          name: 'netlifystrapi',
+          id: '2e28e8ce-e8e6-404e-b5dc-7aa72729cfd8',
+          buildHook:
+            'https://api.netlify.com/build_hooks/640df518be259f1b8cfe229f',
+          branch: 'main', // optional
+        },
+      ],
+    },
   },
 })
